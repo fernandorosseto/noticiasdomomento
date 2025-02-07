72d2519ede555b37cd638b483ba9cf43d359a192
Rosseto <112822062+fernandorosseto@users.noreply.github.com>
2025-02-06 21:36:29 -0300

    uptadeFiles

---
+++ public/js/utils/script.js
@@ -0,0 +1,17 @@
+
+
+function calculateReadingTime(element) {
+  const wordsPerMinute = 200; // Média de leitura: 200 palavras por minuto
+  const textContent = element.textContent;
+  const wordsCount = textContent.trim().split(/\s+/).length;
+  const readingTime = Math.ceil(wordsCount / wordsPerMinute);
+
+  return readingTime;
+}
+
+// Exibe o tempo de leitura aproximado
+const article = document.getElementById('news-article');
+const readingTimeElement = document.getElementById('reading-time');
+const readingTime = calculateReadingTime(article);
+
+readingTimeElement.textContent = `Tempo de leitura aproximado: ${readingTime} minuto(s)`;
